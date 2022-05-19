2cd2e73198c685857aa422555272b1668b8fac71
Jakub Lyczko <jakub.lyczko1@gmail.com>
2022-05-19 12:36:34 +0200

    Add Card widget

--- core-ui/src/components/Extensibility/ds/widgets-details/CardRenderer.js
+++ core-ui/src/components/Extensibility/ds/widgets-details/CardRenderer.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import { LayoutPanel } from 'fundamental-react';
+import { TransTitle } from '@ui-schema/ui-schema/Translate/TransTitle';
 
-export function CardRenderer() {
-  return <div>CardRenderer</div>;
+export function CardRenderer({ schema, storeKeys, widgets, ownKey, ...props }) {
+  const { WidgetRenderer } = widgets;
+  const ownSchema = schema.delete('widget');
+
+  return (
+    <LayoutPanel className="fd-margin--md">
+      <LayoutPanel.Header>
+        <LayoutPanel.Head
+          title={
+            <TransTitle schema={schema} storeKeys={storeKeys} ownKey={ownKey} />
+          }
+        />
+      </LayoutPanel.Header>
+      <WidgetRenderer
+        {...props}
+        storeKeys={storeKeys}
+        schema={ownSchema}
+        widgets={widgets}
+      />
+    </LayoutPanel>
+  );
 }
